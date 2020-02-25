ffd39f90517772d349026ab21a90542d48116930
ullmannV <ullmann.vojta@gmail.com>
2020-02-25 22:01:25 +0100

    Constanst 7Seg

--- main.c
+++ main.c
@@ -13,6 +13,26 @@
 #define CLK 1
 #define DATA 0
 
+/* Znaky 7seg */
+const unsigned char chars_7seg[16] = {
+        0x3F, /* 0 */
+        0x06, /* 1 */
+        0x5B, /* 2 */
+        0x4F, /* 3 */
+        0x66, /* 4 */
+        0x6D, /* 5 */
+        0x7D, /* 6 */
+        0x07, /* 7 */
+        0x7F, /* 8 */
+        0x6F, /* 9 */
+        0x77, /* A */
+        0x7C, /* b */
+        0x39, /* C */
+        0x5E, /* d */
+        0x79, /* E */
+        0x71  /* F */
+};
+
 /* Definice typu Boolean */
 typedef enum {false, true} bool;
 /* Definice typu rezim */
@@ -42,7 +62,7 @@ shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic, enum display 
     for (pozice = 0; pozice < pocet_pozic; ++pozice) {
         /* projdi jednotlive bity a odesli posli je do realneho shift registru */
         int bit;
-        for (bit = 0; bit < BIG_DISP_SIZE; ++bit) {
+        for (bit = 0; bit < SMALL_DISP_SIZE; ++bit) {
             /* Test jestli je bit v log. 1 nebo 0 */
             if (sequence_array[pozice] & bit_masks[bit]) {
                 printf("1"); /*TODO outportb()*/
@@ -72,7 +92,7 @@ main(void)
     do {
         /* Nekonecna smycka programu */
 //      clrscr();
-        unsigned short pole[] = {0xFF2F, 0x003F, 0x55FF, 0x1200, 0xFF24};
+        unsigned short pole[] = {chars_7seg[0], chars_7seg[1], chars_7seg[10], chars_7seg[8], chars_7seg[15]};
         shiftSW(pole, 5, 0);
         program_run = false;
 
