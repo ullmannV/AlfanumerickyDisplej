572946d72e121c15dcdf46513e870e0300252554
ullmannV <ullmann.vojta@gmail.com>
2020-02-26 13:35:31 +0100

    tick()

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <dos.h>
+#include <dos.h>
 
 
 /* velikosti ramcu */
@@ -12,6 +12,7 @@
 #define OUTPUT_PORT 0x300
 #define CLK 1
 #define DATA 0
+#define NEAKTIVITA 0xFF
 
 /* Znaky 7seg */
 const unsigned char chars_7seg[] = {
@@ -43,10 +44,23 @@ const unsigned short bit_masks[WORD_SIZE] =
         {1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8, 1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15};
 
 /*Deklarace globalnich promennych*/
-unsigned char output_buffer;
+static unsigned char output_buffer;
 
 /* Deklarace funkci */
+static inline void tick(void);
 void shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic, enum display rezim);
+
+
+/*
+ * Hodinovy signal
+ */
+static inline void
+tick(void)
+{
+    output_buffer |= 1<<CLK;
+    outportb(OUTPUT_PORT, output_buffer);
+    output_buffer &= ~(1<<CLK);
+}
 
 /*
  * Program posuvny registr
@@ -54,8 +68,11 @@ void shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic, enum dis
 void
 shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic, enum display rezim)
 {
-    /*TODO Start bit*/
+    /* Start Bit */
+    output_buffer |= 1<<DATA;
+    outportb(OUTPUT_PORT, output_buffer);
 
+    tick();
 
     /* projdeme vsechny pozice displeje */
     int pozice;
@@ -65,11 +82,16 @@ shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic, enum display 
         for (bit = 0; bit < SMALL_DISP_SIZE; ++bit) {
             /* Test jestli je bit v log. 1 nebo 0 */
             if (sequence_array[pozice] & bit_masks[bit]) {
-                printf("1"); /*TODO outportb()*/
+                printf("1");
+                output_buffer |= 1<<DATA;
+                outportb(OUTPUT_PORT, output_buffer);
             } else {
                 printf("0");
+                output_buffer &= ~(1<<DATA);
+                outportb(OUTPUT_PORT, output_buffer);
             }
-            /*TODO Tick*/
+
+            tick();
         }
 
         printf("\n");
@@ -81,7 +103,7 @@ int
 main(void)
 {
     /* Program zacina zde */
-
+    output_buffer = NEAKTIVITA;
 
     /* vycisteni obrazovky*/
 //  clrscr()
