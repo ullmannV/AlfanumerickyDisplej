91fe797ea66ba447c9bdbc99b7112686dfe39eb6
ullmannV <ullmann.vojta@gmail.com>
2020-03-03 20:53:10 +0100

    alfanumerak init

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 /* velikosti ramcu */
 #define WORD_SIZE 16
 #define SMALL_DISP_SIZE 7
-#define BIG_DISP_SIZE 14
+#define SMALL_DISP_POZICE 5
+#define BIG_DISP_SIZE 15
+#define BIG_DISP_POZICE 4
 
 /* Konstanty vystupniho portu */
 #define OUTPUT_PORT 0x300
@@ -18,6 +20,7 @@
 #define ESC 0x1B
 #define SPACE 0x20
 #define BACKSPACE 0x08
+#define TAB 0x09
 
 /* Znaky 7seg */
 const unsigned char chars_7seg[] = {
@@ -45,6 +48,9 @@ typedef enum {false, true} bool;
 /* Definice typu rezim */
 enum display {SMALL_SEG, BIG_SEG};
 
+/* Nazev ovladaneho displeje */
+const char *nazev_displeje[] = {"Numericky", "Alfanumericky"};
+
 /* pole masek pro separaci jednotlivych bitu */
 const unsigned short bit_masks[WORD_SIZE] =
 				{1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7, 1 << 8, 1 << 9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15};
@@ -54,8 +60,9 @@ static unsigned char output_buffer;
 
 /* Deklarace funkci */
 void tick(void);
-void shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic);
-
+void shiftSWsmall(const unsigned short *sequence_array);
+void shiftSWbig(const unsigned short *sequence_array);
+void sendStartBit(void);
 
 /*
  * Hodinovy signal
@@ -72,21 +79,18 @@ tick(void)
  * Program posuvny registr
  */
 void
-shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic)
+shiftSWsmall(const unsigned short *sequence_array)
 {
 		int pozice;
 
-		/* Start Bit */
-		output_buffer |= 1<<DATA;
-		outportb(OUTPUT_PORT, output_buffer);
-
-		tick();
+		sendStartBit();
 
 		/* projdeme vsechny pozice displeje */
-		for (pozice = 0; pozice < pocet_pozic; pozice++) {
+		for (pozice = 0; pozice < SMALL_DISP_POZICE; pozice++) {
 				/* projdi jednotlive bity a odesli posli je do realneho shift registru */
 				int bit;
 				for (bit = 0; bit < SMALL_DISP_SIZE; bit++) {
+
 						/* Test jestli je bit v log. 1 nebo 0 */
 						if (sequence_array[pozice] & bit_masks[bit]) {
 								output_buffer |= 1<<DATA;
@@ -96,28 +100,76 @@ shiftSW(unsigned short *sequence_array, unsigned char pocet_pozic)
 								outportb(OUTPUT_PORT, output_buffer);
 						}
 
-						tick();
+						tick(); /* Odesli bit ven */
 				}
-
-				printf("\n");
 		}
 }
 
+void
+shiftSWbig(const unsigned short *sequence_array)
+{
+    //int opakovani;
+    /* projdeme vsechny pozice displeje */
+    for (int opakovani = 0; opakovani < 2; opakovani++) {
+        //int pozice;
+        sendStartBit();
+
+        for (int pozice = opakovani; pozice < BIG_DISP_POZICE; pozice+=2) {
+            /* projdi jednotlive bity a odesli posli je do realneho shift registru */
+            int bit;
+            for (bit = 0; bit < BIG_DISP_SIZE; bit++) {
+
+                /* Test jestli je bit v log. 1 nebo 0 */
+                if (sequence_array[pozice] & bit_masks[bit]) {
+                    output_buffer |= 1<<DATA;
+                    outportb(OUTPUT_PORT, output_buffer);
+                } else {
+                    output_buffer &= ~(1<<DATA);
+                    outportb(OUTPUT_PORT, output_buffer);
+                }
+
+                tick(); /* Odesli bit ven */
+            }
+        }
+        /* dopln zbivajici bity */
+        /* Urceni pozice */
+
+        /* bity bez vyznamu */
+
+
+        delay(10); /* Pauza pro ocko */
+    }
+
+}
+
+void
+sendStartBit(void)
+{
+    /* Start Bit */
+    output_buffer |= 1<<DATA;
+    outportb(OUTPUT_PORT, output_buffer);
+
+    tick(); /* Odesli start bit */
+}
 
 int
 main(void)
 {
 		bool program_run;
+		enum display rezim = SMALL_SEG;
 		unsigned char cursor = 0;
-		unsigned short pole[5];
-		pole[0] = chars_7seg[10];
-		pole[1] = chars_7seg[15];
-		pole[2] = chars_7seg[15];
-		pole[3] = chars_7seg[15];
-		pole[4] = chars_7seg[15];
+		unsigned short zobrazovane_znaky[5];
+        zobrazovane_znaky[0] = chars_7seg[10];
+        zobrazovane_znaky[1] = chars_7seg[15];
+        zobrazovane_znaky[2] = chars_7seg[15];
+        zobrazovane_znaky[3] = chars_7seg[15];
+        zobrazovane_znaky[4] = chars_7seg[15];
+
 		/* Program zacina zde */
+		/* vycisteni dat na portu */
 		output_buffer = NEAKTIVITA;
 		outportb(OUTPUT_PORT, output_buffer);
+
 		/* vycisteni obrazovky*/
 		clrscr();
 
@@ -129,8 +181,11 @@ main(void)
 				int hitted_key;
 
 				/* Nekonecna smycka programu */
+
+				/* Informace na konzoli */
 				clrscr();
-				printf("Zapises na %d. pozici", cursor+1);
+				printf("Ovladas %s displej \n", nazev_displeje[rezim]);
+				printf("Zapises na %d. pozici \n", cursor+1);
 
 				if (kbhit()) {
 					hitted_key = getch();
@@ -141,105 +196,105 @@ main(void)
 					program_run = false;
 					break;
 				case '0':
-					pole[cursor] = chars_7seg[0];
+                    zobrazovane_znaky[cursor] = chars_7seg[0];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '1':
-					pole[cursor] = chars_7seg[1];
+                    zobrazovane_znaky[cursor] = chars_7seg[1];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '2':
-					pole[cursor] = chars_7seg[2];
+                    zobrazovane_znaky[cursor] = chars_7seg[2];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '3':
-					pole[cursor] = chars_7seg[3];
+                    zobrazovane_znaky[cursor] = chars_7seg[3];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '4':
-					pole[cursor] = chars_7seg[4];
+                    zobrazovane_znaky[cursor] = chars_7seg[4];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '5':
-					pole[cursor] = chars_7seg[5];
+                    zobrazovane_znaky[cursor] = chars_7seg[5];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '6':
-					pole[cursor] = chars_7seg[6];
+                    zobrazovane_znaky[cursor] = chars_7seg[6];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '7':
-					pole[cursor] = chars_7seg[7];
+                    zobrazovane_znaky[cursor] = chars_7seg[7];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '8':
-					pole[cursor] = chars_7seg[8];
+                    zobrazovane_znaky[cursor] = chars_7seg[8];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case '9':
-					pole[cursor] = chars_7seg[9];
+                    zobrazovane_znaky[cursor] = chars_7seg[9];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'a':
-					pole[cursor] = chars_7seg[10];
+                    zobrazovane_znaky[cursor] = chars_7seg[10];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'b':
-					pole[cursor] = chars_7seg[11];
+                    zobrazovane_znaky[cursor] = chars_7seg[11];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'c':
-					pole[cursor] = chars_7seg[12];
+                    zobrazovane_znaky[cursor] = chars_7seg[12];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'd':
-					pole[cursor] = chars_7seg[13];
+                    zobrazovane_znaky[cursor] = chars_7seg[13];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'e':
-					pole[cursor] = chars_7seg[14];
+                    zobrazovane_znaky[cursor] = chars_7seg[14];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case 'f':
-					pole[cursor] = chars_7seg[15];
+                    zobrazovane_znaky[cursor] = chars_7seg[15];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case SPACE:
-					pole[cursor] = chars_7seg[16];
+                    zobrazovane_znaky[cursor] = chars_7seg[16];
 					cursor++;
 					hitted_key = NULL;
 					break;
 				case BACKSPACE:
 					if (cursor == 0)
 						cursor = 5;
-					pole[--cursor] = chars_7seg[16];
+                        zobrazovane_znaky[--cursor] = chars_7seg[16];
 					hitted_key = NULL;
 					break;
 
 				}
 				/* osetreni kurzoru  */
-				if (cursor > 4)
+				if (cursor > SMALL_DISP_POZICE - 1)
 					cursor = 0;
 
-				shiftSW(pole, 5);
+				shiftSWsmall(zobrazovane_znaky);
 
-				delay(100);
+				/*delay(100);*/
 		} while (program_run);
 
 		return 0;
